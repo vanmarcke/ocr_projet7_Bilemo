5a9defea5e2a6192b4cce8c7d1d34afa111c7154
Frédéric <fvanmarcke72@gmail.com>
2022-03-20 18:39:42 +0100

    Service responsible for standardizing information for the exception type

---
+++ src/EventSubscriber/ExceptionListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $response = new JsonResponse();
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $message = json_encode([
+            'error' => $exception->getMessage(),
+            'code' => $response->getStatusCode(),
+        ]);
+
+        $response->setContent($message);
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
